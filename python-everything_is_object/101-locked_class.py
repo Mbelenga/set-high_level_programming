f7d5ae5621e5ba71ba1e33da7b91cc4e031daff0
Mbelenga <mbelengaterence@gmail.com>
2026-08-02 16:52:05 +0300

    Low memory cost

---
+++ python-everything_is_object/101-locked_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+"""
+define a class called LockedClass
+"""
+
+
+class LockedClass:
+    """
+    Locks the class for only 1 attribute
+    """
+    __slots__ = ['first_name']
