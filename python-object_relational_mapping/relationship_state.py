702a655614d79e67ad705387e92f4b9097f236cc
Mbelenga <mbelengaterence@gmail.com>
2026-08-18 23:13:11 +0300

    Fixing the error

--- python-object_relational_mapping/relationship_state.py
+++ python-object_relational_mapping/relationship_state.py
@@ -5,8 +5,10 @@ Module that connects to the database
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from relationship_city import Base, City
 
-Base = declarative_base()
+# Base = declarative_base()
 
 
 class State(Base):
@@ -14,3 +16,5 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
+
+    cities = relationship("City", backref="state", cascade="all, delete")
