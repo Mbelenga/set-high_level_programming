36eb20bd41dcc13adb4b652e9917ed633087dd1b
Mbelenga <mbelengaterence@gmail.com>
2026-08-23 00:40:29 +0300

    Find a Peak

---
+++ python-network_0/6-peak.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Finds a peak in an unsorted integers"""
+
+
+def find_peak(list_of_integers):
+    """Returns a peak in a list of unsorted integers"""
+    if list_of_integers == []:
+        return None
+
+    size = len(list_of_integers)
+    if size == 1;
