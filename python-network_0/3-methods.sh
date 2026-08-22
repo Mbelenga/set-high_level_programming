9aeb0cb604524499da0130d886b956df718d61dd
Mbelenga <mbelengaterence@gmail.com>
2026-08-23 00:33:01 +0300

    cURL Only Methods

--- python-network_0/3-methods.sh
+++ python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# a Bash script that takes in a URL and displays all HTTP methods the server will accept.
+# a Bash script that takes in a URL and displays all HTTP methods
 curl -Is "$1" | grep Allow | cut -c 8-
